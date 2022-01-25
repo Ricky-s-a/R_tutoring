b8670c18c484ac0b2fc00e035c566b3240832291
Ricky-s-a <rikitamago315@gmail.com>
2022-01-25 21:17:23 +0900

    push a report on `typhoon`

--- R/typhoon.R
+++ R/typhoon.R
@@ -1,26 +1,7 @@
-path <- "~/R/project/R_tutoring/Git/R_tutoring/Data"
+# get the url
+url <- "https://raw.githubusercontent.com/Ricky-s-a/R_tutoring/main/Data/typhoon.csv"
 
-typhoon <- read.csv(path)
-
-# reset names of the columns 
-columns <- c("year", month.abb, "annual_sum")
-typhoon <- `names<-`(typhoon, columns)
-
-# check
-head(typhoon)
-
-
-# no. of typhoons hitting japan each month
-y <- apply(typhoon[1:70, 2:13], 2, sum, na.rm = TRUE)
-
-# plot a bar plot
-barplot(y, 
-        ylim=c(0,80), 
-        xlab='Month', 
-        ylab = 'No. of Typhoons',
-        main='Typhoons Hitting Japan Each Month')
-
-typhoon <- read.csv(path)
+typhoon <- read.csv(url)
 
 # reset names of the columns 
 columns <- c("year", month.abb, "annual_sum")
@@ -39,3 +20,4 @@ barplot(y,
         xlab='Month', 
         ylab = 'No. of Typhoons',
         main='Typhoons Hitting Japan Each Month')
+
