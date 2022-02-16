822d071134bc2074b753d8ce662b7f41642121a9
Ricky-s-a <rikitamago315@gmail.com>
2022-02-16 18:49:36 +0900

    set gradient colors for the mapping

--- R/kadai01.R
+++ R/kadai01.R
@@ -1,14 +1,21 @@
 
+library(tidyverse)
 
 # load file ---------------------------------------------------------------
 
 path <- "~/R/project/R_tutoring/Git/R_tutoring/Data/kadai01.csv"
-pacman::p_load("tidyverse")
 
 df <- read_csv(path, locale = locale(encoding = "UTF-8"))
-df <- read_csv(path, locale = locale(encoding = "shift-jis"))
 
-df <- read_delim(path, local = "UTF-8")
+df <- df %>% 
+  mutate(pref = c("Hokkaido", "Aomori", "Iwate", "Miyagi", "Akita", "Yamagata", 
+   "Fukushima", "Ibaraki", "Tochigi", "Gunma", "Saitama", "Chiba", 
+   "Tokyo", "Kanagawa", "Niigata", "Toyama", "Ishikawa", "Fukui", 
+   "Yamanashi", "Nagano", "Gifu", "Shizuoka", "Aichi", "Mie", "Shiga", 
+   "Kyoto", "Osaka", "Hyogo", "Nara", "Wakayama", "Tottori", "Shimane", 
+   "Okayama", "Hiroshima", "Yamaguchi", "Tokushima", "Kagawa", "Ehime", 
+   "Kochi", "Fukuoka", "Saga", "Nagasaki", "Kumamoto", "Oita", "Miyazaki", 
+   "Kagoshima", "Okinawa")
+  )
 
-
-  head(df)
+df
